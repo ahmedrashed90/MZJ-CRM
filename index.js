94cf992a89acdf34982953d230e5e235de3a9466
ahmedrashed90 <134849433+ahmedrashed90@users.noreply.github.com>
2026-02-03 23:01:19 +0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
-import serverless from "serverless-http";
-import app from "../server.js";
-
-export const config = { api: { bodyParser: { sizeLimit: "1mb" } } };
-
-export default serverless(app);
